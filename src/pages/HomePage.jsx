db2e73aaf3de80a011630180f6c802a86a840dab
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-04-11 14:39:24 +0530

    Add server proxy config for /api and /images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,16 +6,18 @@ import './HomePage.css'
 export function HomePage() {
 
     const [products , setProduct] = useState([]);
-    const [cart , setCart] = useState();
+    const [cart , setCart] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        // axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 // console.log(response.data);
                 setProduct(response.data);
             });
         
-        axios.get('http://localhost:3000/api/cart-items')
+        // axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then( (response) => {
                 setCart(response.data);
             })
