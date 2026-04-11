46dd66b0827ca7f174e4d3b90f6607a5b4fb271e
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-04-11 14:58:18 +0530

    Lift cart up and share it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,10 @@ import { useEffect , useState } from 'react';
 import { Header } from '../components/Header.jsx'
 import './HomePage.css'
 
-export function HomePage() {
+export function HomePage( { cart } ) {
 
     const [products , setProduct] = useState([]);
-    const [cart , setCart] = useState([]);
-
+    
     useEffect(() => {
         // axios.get('http://localhost:3000/api/products')
         axios.get('/api/products')
@@ -15,12 +14,6 @@ export function HomePage() {
                 // console.log(response.data);
                 setProduct(response.data);
             });
-        
-        // axios.get('http://localhost:3000/api/cart-items')
-        axios.get('/api/cart-items')
-            .then( (response) => {
-                setCart(response.data);
-            })
     } , []);
 
     return (
