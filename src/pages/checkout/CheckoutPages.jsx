25be9d62c27ceac2f6edf1abb0a68f5a3868aa35
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-04-18 21:24:50 +0530

    Convert home and checkout to async await

--- src/pages/checkout/CheckoutPages.jsx
+++ src/pages/checkout/CheckoutPages.jsx
@@ -11,15 +11,19 @@ export function CheckoutPage({ cart }) {
     const [paymentSummary, setPaymentSummary] = useState(null);
 
     useEffect(() => {
-        axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
-            .then((response) => {
-                setDeliveryOptions(response.data);
-            });
 
-        axios.get('/api/payment-summary')
-            .then((response) => {
-                setPaymentSummary(response.data);
-            })
+        const fetchCheckoutData = async () => {
+            let response = await axios.get(
+                '/api/delivery-options?expand=estimatedDeliveryTime'
+            );
+            setDeliveryOptions(response.data);
+
+            response = await axios.get('/api/payment-summary')
+            setPaymentSummary(response.data);
+        };
+
+        fetchCheckoutData();
+
     }, []);
 
     return (
@@ -34,9 +38,9 @@ export function CheckoutPage({ cart }) {
 
                 <div className="checkout-grid">
 
-                    <OrderSummary cart={ cart } deliveryOptions={ deliveryOptions }/>
+                    <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
 
-                    <PaymentSummary paymentSummary={ paymentSummary }/>
+                    <PaymentSummary paymentSummary={paymentSummary} />
 
                 </div>
             </div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -10,11 +10,19 @@ export function HomePage( { cart } ) {
     
     useEffect(() => {
         // axios.get('http://localhost:3000/api/products')
-        axios.get('/api/products')
-            .then((response) => {
-                // console.log(response.data);
-                setProduct(response.data);
-            });
+    //     axios.get('/api/products')
+    //         .then((response) => {
+    //             // console.log(response.data);
+    //             setProduct(response.data);
+    //         });
+    // } , []);
+
+        const getHomeData = async() => {
+            const response = await axios.get('/api/products');
+            setProduct(response.data);   
+        }
+
+        getHomeData();
     } , []);
 
     return (
